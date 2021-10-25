b5d765dafd788506cdc1b014cce7110f6008da0d
Garland Frye <Garland.Frye@match.com>
2021-10-25 16:37:39 -0500

    next batch

---
+++ Landmarks/Landmarks/Views/PageView/PageView.swift
@@ -0,0 +1,28 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by Garland Frye on 10/25/21.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+
+    var body: some View {
+        PageViewController(pages: pages)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(
+            pages: ModelData().features.map {
+                FeatureCard(landmark: $0)
+            }
+        )
+        .aspectRatio((3 / 2), contentMode: .fit)
+        
+    }
+}
